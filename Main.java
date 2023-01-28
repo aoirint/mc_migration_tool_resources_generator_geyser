c938d589eb05f93dfe323bc20e2f2f873717429f
aoirint <aoirint@gmail.com>
2023-01-28 20:00:05 +0900

    \r\n

--- Main.java
+++ Main.java
@@ -3,12 +3,12 @@ import org.geysermc.geyser.inventory.item.Enchantment;
 
 public class Main {
   public static void main(String[] argv) {
-    System.out.printf("java_numeric_identifier,java_string_identifier,bedrock_numeric_identifier\n");
+    System.out.printf("java_numeric_identifier,java_string_identifier,bedrock_numeric_identifier\r\n");
     for (final var bedrockEnchantment: Enchantment.values()) {
       final var bedrockNumericIdentifier = bedrockEnchantment.ordinal();
       final var javaStringIdentifier = "minecraft:" + bedrockEnchantment.name().toLowerCase(Locale.ENGLISH);
       final var javaNumericIdentifier = Enchantment.JavaEnchantment.getByJavaIdentifier(javaStringIdentifier).ordinal();
-      System.out.printf("%d,%s,%d\n", javaNumericIdentifier, javaStringIdentifier, bedrockNumericIdentifier);
+      System.out.printf("%d,%s,%d\r\n", javaNumericIdentifier, javaStringIdentifier, bedrockNumericIdentifier);
     }
   }
 }
