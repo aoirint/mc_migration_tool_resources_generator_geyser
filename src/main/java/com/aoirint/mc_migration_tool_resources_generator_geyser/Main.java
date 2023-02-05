6c19dce4eb037a1acb4e61351b663fa9d48f14b7
aoirint <aoirint@gmail.com>
2023-02-05 16:52:53 +0900

    fix column order

--- src/main/java/com/aoirint/mc_migration_tool_resources_generator_geyser/Main.java
+++ src/main/java/com/aoirint/mc_migration_tool_resources_generator_geyser/Main.java
@@ -5,7 +5,7 @@ import org.geysermc.geyser.inventory.item.Enchantment;
 
 public class Main {
   public static void main(String[] argv) {
-    System.out.printf("bedrock_numeric_identifier,java_numeric_identifier,java_string_identifier\r\n");
+    System.out.printf("bedrock_numeric_identifier,java_string_identifier,java_numeric_identifier\r\n");
     for (final var bedrockEnchantment: Enchantment.values()) {
       final var bedrockNumericIdentifier = bedrockEnchantment.ordinal();
       final var javaStringIdentifier = "minecraft:" + bedrockEnchantment.name().toLowerCase(Locale.ENGLISH);
